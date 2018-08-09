5db7b66da57b9b63c88e39063ee8aecba3b6d38c
Ahmed Gad <ahmed.f.gad@gmail.com>
2018-08-10 00:19:52 +0200

    Update NumPyCNN.py

--- NumPyCNN.py
+++ NumPyCNN.py
@@ -82,14 +82,14 @@ def conv(img, conv_filter):
 
 def pooling(feature_map, size=2, stride=2):
     #Preparing the output of the pooling operation.
-    pool_out = numpy.zeros((numpy.uint16((feature_map.shape[0]-size+1)/stride),
-                            numpy.uint16((feature_map.shape[1]-size+1)/stride),
+    pool_out = numpy.zeros((numpy.uint16((feature_map.shape[0]-size+1)/stride+1),
+                            numpy.uint16((feature_map.shape[1]-size+1)/stride+1),
                             feature_map.shape[-1]))
     for map_num in range(feature_map.shape[-1]):
         r2 = 0
-        for r in numpy.arange(0,feature_map.shape[0]-size-1, stride):
+        for r in numpy.arange(0,feature_map.shape[0]-size+1, stride):
             c2 = 0
-            for c in numpy.arange(0, feature_map.shape[1]-size-1, stride):
+            for c in numpy.arange(0, feature_map.shape[1]-size+1, stride):
                 pool_out[r2, c2, map_num] = numpy.max([feature_map[r:r+size,  c:c+size]])
                 c2 = c2 + 1
             r2 = r2 +1
