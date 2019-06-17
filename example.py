3752290f5e56b1b8629c7d851f9bcea9a07e33af
Ahmed Gad <ahmed.f.gad@gmail.com>
2019-06-18 00:46:32 +0200

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import matplotlib
 import NumPyCNN as numpycnn
 
 """
-Convolutional neural network implementation using NumPy.
+Convolutional neural network implementation using NumPy
 An article describing this project is titled "Building Convolutional Neural Network using NumPy from Scratch". It is available in these links: https://www.linkedin.com/pulse/building-convolutional-neural-network-using-numpy-from-ahmed-gad/
 https://www.kdnuggets.com/2018/04/building-convolutional-neural-network-numpy-scratch.html
 It is also translated into Chinese: http://m.aliyun.com/yunqi/articles/585741
